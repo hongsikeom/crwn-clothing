3748e50a58440f652b8a2a6a68afe687765dd5d0
Hongsik Eom <haramang76@gmail.com>
2020-12-25 00:18:26 -0500

    Created project with creat react app added files for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import "./homepage.styles.scss";
+
+const HomPage = () => (
+    <div className= "homepage">
+        <div className = 'directory-menu'>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 clasName = 'title' HATS>HATS</h1>
+                    <span className = 'subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 clasName = 'title' HATS>JACKETS</h1>
+                    <span className = 'subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 clasName = 'title' HATS>SNEAKERS</h1>
+                    <span className = 'subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 clasName = 'title' HATS>WOMENS</h1>
+                    <span className = 'subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 clasName = 'title' HATS>MENS</h1>
+                    <span className = 'subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomPage;
